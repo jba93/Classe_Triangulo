caeaeef1c38bbad9e276387704357edcfd14a1cc
Juliana <29216050+jba93@users.noreply.github.com>
2017-09-26 10:49:49 -0300

    Update Classe Triângulo.py

--- Classe Triângulo.py
+++ Classe Triângulo.py
@@ -46,6 +46,7 @@ class Triangulo:
 def main():
     t1 = Triangulo(5, 4, 3)
     t2 = Triangulo(10, 8, 6)
+    
     print ("Triângulo 1")
     print ("Lado a =", t1.a)
     print ("Lado b =", t1.b)
@@ -53,15 +54,15 @@ def main():
     print ("Perímetro =", t1.perimetro(t1))
     print ("Tipo do triângulo: ", t1.tipo_lado(t1))
     print ("É retângulo? ",t1.retangulo(t1))
-    print ()
-    print ("Triângulo 2")
+ 
+    print ("\nTriângulo 2")
     print ("Lado a =", t2.a)
     print ("Lado b =", t2.b)
     print ("Lado a =", t2.c)
     print ("Perímetro =", t2.perimetro(t2))
     print ("Tipo do triângulo: ", t2.tipo_lado(t2))
     print ("É retângulo? ",t2.retangulo(t2))
-    print ()
-    print ("Os triângulos 1 e 2 são semelhantes? ", t1.semelhantes(t2))
+    
+    print ("\nOs triângulos 1 e 2 são semelhantes? ", t1.semelhantes(t2))
 
 main()
